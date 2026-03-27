2b533170cead09a013d149d01977bac43cb499da
Sharon Furtado <2000049@XTSWORLD.LOCAL>
2026-03-27 18:58:25 +0530

    switch to single DATABASE_URL env var

--- database.py
+++ database.py
@@ -3,22 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
- 
+
 load_dotenv()
- 
-USER = os.getenv("user")
-PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
- 
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
- 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
- 
- 
+
+
 def get_db():
     db = SessionLocal()
     try:
